bbfa243096260a651f95abea440895ac6b8b1c28
DenisineD-creator <dburobin1@mail.ru>
2024-08-29 17:29:37 +0300

    Add files via upload

---
+++ Total_part_1.py
@@ -0,0 +1,6 @@
+grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+student = {'Johnny','Bilbo','Steven','Khendrik','Aaron'}
+student = set(list({'Johnny','Bilbo','Steven','Khendrik','Aaron'}))
+print(student)
+grades = (set[[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]])
+print(grades)
